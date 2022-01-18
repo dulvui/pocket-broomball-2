e11351fa6aed08329a1b4c9d38a8df8c5901ea5b
dulvui <dulvui@posteo.de>
2022-01-18 22:56:26 +0100

    add A/B buttons and player looks to ball

--- game/nodes/actors/player/Player.gd
+++ game/nodes/actors/player/Player.gd
@@ -22,29 +22,14 @@ func _ready():
 	direction = (ball.global_position - global_position).normalized()
 
 func _physics_process(delta):
-#	if destination != null:
-#		direction = (destination - global_position).normalized()
-#		var distance_to_player = global_position.distance_to(destination)
-#		move_and_slide(direction * speed * distance_to_player)
-
-	if joystickLeft and joystickLeft.is_pressed():
-		position += joystickLeft.get_output() * speed * delta
-		direction = joystickLeft.get_output().angle()
-		look_at(direction)
-		
-	
+			
 	# Movement using Input functions:
 	var move := Vector2.ZERO
 	move.x = Input.get_axis("ui_left", "ui_right")
 	move.y = Input.get_axis("ui_up", "ui_down")
 	
 	move_and_slide(move * speed)
-	
-#func _process(delta: float) -> void:
-#	# Movement using the joystick output:
-
-	
-	# Rotation:
+	look_at(ball.global_position)
 
 
 func _on_ChargeDetector_body_entered(body):
